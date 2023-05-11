9c77f5f74c646efb339880bea3311b3996f11794
Luis Perdigao <perdigao1@yahoo.com>
2023-05-11 15:01:14 +0100

    improved after testing

--- src/okapi_em/_tests/test_slice_alignment.py
+++ src/okapi_em/_tests/test_slice_alignment.py
@@ -12,7 +12,7 @@ def test_cl_affineTransform():
     lin_tr = np.eye(2)
     transl_tr=np.array([0,0])
 
-    myAf0 = slice_alignment.affineTransform(lin_tr, transl_tr)
+    myAf0 = slice_alignment.cAffineTransform(lin_tr, transl_tr)
 
     #Apply transform and check result
     p_res = myAf0.applyToPoint(points)
@@ -22,14 +22,14 @@ def test_cl_affineTransform():
 
     #rotation 90 degrees
     lin_tr = np.array([[ 0, -1], [1, 0]])
-    myAf1 = slice_alignment.affineTransform(lin_tr, transl_tr)
+    myAf1 = slice_alignment.cAffineTransform(lin_tr, transl_tr)
     p_res = myAf1.applyToPoint(points)
     assert p_res[0]==-points[1]
     assert p_res[1]==points[0]
 
     #Translation
     transl_tr=randint(10,20,size=2)
-    myAf2 = slice_alignment.affineTransform(lin_tr, transl_tr)
+    myAf2 = slice_alignment.cAffineTransform(lin_tr, transl_tr)
     p_res = myAf2.applyToPoint(points)
     assert p_res[0]==-points[1]+transl_tr[0]
     assert p_res[1]==points[0]+transl_tr[1]
